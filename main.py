94850d1cae07a6c866e8b803f13777dafd9696ff
Алексей <moroz-aleksey-95@mail.ru>
2023-01-22 18:30:04 +1000

    1

---
+++ main.py
@@ -0,0 +1,10 @@
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel
+
+
+class ballon_calc(MDApp):
+    def build(self):
+        return MDLabel(text="Hello, World", halign="center")
+
+
+ballon_calc().run()
